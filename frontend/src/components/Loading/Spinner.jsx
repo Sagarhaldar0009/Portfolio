54666da398049ce2b255390208cd194e20d1670d
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2024-09-06 00:51:32 +0530

    Spinner component updates

--- frontend/src/components/Loading/Spinner.jsx
+++ frontend/src/components/Loading/Spinner.jsx
@@ -4,8 +4,9 @@ import "./Spinner.css"
 
 const Spinner = () => {
   return (
-    <div className='w-screen h-[calc(100vh-80px)] flex justify-center items-center'>
+    <div className='w-screen h-[calc(100vh-80px)] flex flex-col justify-center items-center'>
       <div className='spinner'></div>
+      <div className='text-white'>Wait a Minute, while the data is Loading...</div>
     </div>
    
   )
