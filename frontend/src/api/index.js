bd1c53f821d35a093ed2bbf507a706d38a188967
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2024-09-06 01:02:56 +0530

    Loading Message is Updated

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://portfolio-npib.onrender.com/api', // Adjust the baseURL to your backend
+  // baseURL : 'http://localhost:4000/api',
 });
 
 export default API;
--- frontend/src/components/Loading/Spinner.jsx
+++ frontend/src/components/Loading/Spinner.jsx
@@ -4,9 +4,9 @@ import "./Spinner.css"
 
 const Spinner = () => {
   return (
-    <div className='w-screen h-[calc(100vh-80px)] flex flex-col justify-center items-center'>
+    <div className='w-screen h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-y-4'>
       <div className='spinner'></div>
-      <div className='text-white'>Wait a Minute, while the data is Loading...</div>
+      <div className='text-white font-bold text-xl'>Please wait a minute, while the data is being fetched...</div>
     </div>
    
   )
