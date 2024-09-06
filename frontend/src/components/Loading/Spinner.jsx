84b192fb6a99c8ea34487d71539a625d23c8fa8e
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2024-09-06 08:14:11 +0530

    Loading Message is align centered

--- frontend/src/components/Loading/Spinner.jsx
+++ frontend/src/components/Loading/Spinner.jsx
@@ -6,7 +6,7 @@ const Spinner = () => {
   return (
     <div className='w-screen h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-y-4'>
       <div className='spinner'></div>
-      <div className='text-white font-bold text-xl'>Please wait a minute, while the data is being fetched...</div>
+      <div className='text-white font-bold text-xl text-center'><p>Please wait a minute, while the data is being fetched...</p></div>
     </div>
    
   )
